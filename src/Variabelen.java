ce753feb1b2c587db00ed9e8cb825f2d6db11cff
Roel Brouwer <roelbrouwer@msn.com>
2014-06-23 00:14:42 +0200

    Opzet CSP, aanpassingen verslag

---
+++ src/Variabelen.java
@@ -0,0 +1,70 @@
+public class Variabelen implements IVakje {
+	int n;
+	boolean[] domein;
+	// "Coordinaten" van het vakje.
+	int x, y;
+	
+	public Variabelen(int n, boolean[] domein, int x, int y) {
+		this.n = n;
+		this.domein = domein;
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getWaarde() {
+		return n;
+	}
+	
+	public void setWaarde(int n) {
+		this.n = n;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public boolean getVariabel() {
+		return true;
+	}
+	
+	public boolean gevuld() {
+		if (n == 0)
+			return false;
+		else
+			return true;
+	}
+	
+	public void domeinelementToevoegen(int x) {
+		domein[x - 1] = true;
+	}
+	
+	public void domeinelementVerwijderen(int x) {
+		domein[x - 1] = false;
+	}
+	
+	public boolean elementInDomein(int x) {
+		return domein[x - 1];
+	}
+	
+	// Geeft terug hoeveel getallen nog in het domein zitten.
+	public int domeinGrootte() {
+		int count = 0;
+		for (int i = 0; i < domein.length; i++)
+		{
+			if (domein[i])
+			{
+				count++;
+			}
+		}
+		return count;
+	}
+	
+	public String toString()
+	{
+		return Integer.toString(n);
+	}
+}
